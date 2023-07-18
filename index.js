d722bf7517f6ecad984abba201ad6a7474e12b9f
IBBUKHAN <115185488+IBBUKHAN@users.noreply.github.com>
2023-07-18 19:01:23 +0530

    testing

--- index.js
+++ index.js
@@ -1,60 +1,54 @@
-const fs = require('fs');
-const PDFParser = require('pdf-parse');
-const axios = require('axios');
-
-const OPENAI_API_KEY = ''; // Replace with your OpenAI API key
-const OPENAI_API_URL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
-const SAMPLE_PDF_FILE = 'khan.pdf'; // Path to your sample PDF file
-
-const QUERY = `
-Want to extract information about "Harishena composed a prashasti in praise of Gautamiputra Shri Satakarni.".
-Return the results in JSON format without any explanation.
-The text related to "Harishena composed a prashasti in praise of Gautamiputra Shri Satakarni." is as follows:
-%s
-`;
-
-async function extractPDFContent(pdfPath) {
-  const pdfData = fs.readFileSync(pdfPath);
-  const pdf = await PDFParser(pdfData);
-  return pdf.text;
-}
-
-async function callOpenAIAPI(pdfText) {
-  const requestBody = {
-    prompt: QUERY.replace('%s', pdfText),
-    temperature: 0.5,
-    max_tokens: 2048,
-  };
-
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${OPENAI_API_KEY}`,
-    },
-  };
-
-  const response = await axios.post(OPENAI_API_URL, requestBody, config);
-  return response.data.choices[0].text;
-}
-
-function extractAnswer(apiResponse) {
-  try {
-    const answer = JSON.parse(apiResponse);
-    return answer;
-  } catch (error) {
-    return {};
-  }
-}
+const axios = require("axios");
+const fs = require("fs");
+const json = require("./ibbu.json");
 
 async function main() {
+  const url = "https://sebi.corover.ai/sebiAPI/nlp/answer/hi";
+  const headers = {
+    appId: "b3540cfc-7d56-4478-b913-56c6b8baaf7f",
+    "Content-Type": "application/json",
+  };
+
+  const output = [];
+
   try {
-    const pdfPath = `C:/Users/Ibbu/Downloads/khan.pdf`;
-    const pdfText = await extractPDFContent(pdfPath);
-    const apiResponse = await callOpenAIAPI(pdfText);
-    const results = extractAnswer(apiResponse);
-    console.log(results);
+    for (let i = 0; i < json.length; i++) {
+      const question = json[i].question;
+      const answer = json[i].answer;
+
+      const data = {
+        query: question,
+        source:
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36",
+        inputType: "TEXT",
+        userToken: "94426fe5-eed4-4ff2-b3df-0a4dba0f1e3a",
+      };
+
+      try {
+        const response = await axios.post(url, data, { headers });
+        const responseData = response.data.answer.response;
+        const translatevalue = response.data.answer.TranslatedQuery;
+
+        const queryResponse = {
+          query: question,
+          Translate: translatevalue,
+          old_response: responseData,
+          new_response: answer,
+        };
+
+        output.push(queryResponse);
+      } catch (error) {
+        console.log(
+          `Error for question: "${question}". Moving to the next question.`
+        );
+        // console.error(error);
+      }
+    }
+
+    fs.writeFileSync("queryResponse.json", JSON.stringify(output));
+    console.log("Query and response data saved to queryResponse.json file.");
   } catch (error) {
-    console.error('An error occurred:', error);
+    console.error(error);
   }
 }
 
